192049ff7d0d9b702574548547b59b9075b1aa3d
code1990 <s1332177151@sina.com>
2020-02-29 21:13:22 +0800

    15

--- phonebook/client/phonebook/src/pages/components/Alphabet.vue
+++ phonebook/client/phonebook/src/pages/components/Alphabet.vue
@@ -2,13 +2,25 @@
     <ul class="list">
       <li class="item"
           @click="handleLetterClick"
-         v-for="(item,key) of letters">{{item}}</li>
+          @touchstart="handleTouchStart"
+          @touchmove="handleTouchMove"
+          @touchend="handleTouchEnd"
+          v-for="(item,key) of letters"
+          :key="item"
+          :ref="item">{{item}}</li>
     </ul>
 </template>
 
 <script>
     export default {
         name: "Alphabet",
+        data(){
+          return{
+            touchStatus:false,
+            startY:0,
+            timer:null
+          }
+        },
         props:{
           phoneBooks:Object
         },
@@ -26,8 +38,34 @@
           handleLetterClick(e){
             console.log(e.target.innerText)
             this.$emit("change",e.target.innerText)
+          },
+          handleTouchStart(){
+            this.touchStatus=true
+            console.log("start")
+          },
+          handleTouchMove(e){
+            console.log("move")
+            if(this.timer){
+              clearTimeout(this.timer)
+            }
+            this.timer=setTimeout(()=>{
+              if(this.touchStatus){
+                const touchY=e.touches[0].clientY-79
+                const index=Math.floor((touchY-this.startY)/20)
+                if(index>=0 && index<this.letters.length){
+                  this.$emit("change",this.letters[index])
+                }
+              }
+            },20)
+          },
+          handleTouchEnd(){
+            this.touchStatus=false
+            console.log("end")
           }
-      }
+        },
+        updated() {
+          this.startY=this.$refs['A'][0].offsetTop
+        }
     }
 </script>
 
