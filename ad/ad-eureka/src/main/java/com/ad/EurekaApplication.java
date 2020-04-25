c73cd3d9928f8d7429cb78d288a4d884658f818f
code1990 <s1332177151@sina.com>
2020-04-25 12:28:59 +0800

    init

---
+++ ad/ad-eureka/src/main/java/com/ad/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @program: ad
+ * @Date: 2020-04-25 11:23
+ * @Author: code1990
+ * @Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
