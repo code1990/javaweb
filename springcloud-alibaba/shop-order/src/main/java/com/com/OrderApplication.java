19f229a27858c647b045400c4d8e8c13421da88c
code1990 <s1332177151@sina.com>
2020-06-11 23:25:26 +0800

    init

---
+++ springcloud-alibaba/shop-order/src/main/java/com/com/OrderApplication.java
@@ -0,0 +1,18 @@
+package com.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud-alibaba
+ * @Date: 2020-06-11 23:21
+ * @Author: code1990
+ * @Description:
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class);
+    }
+
+}
