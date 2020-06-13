4e3a4d456bde68ec00b6dee15f2cdfcfdfc99b92
code1990 <s1332177151@sina.com>
2020-06-13 23:07:54 +0800

    init

--- springcloud-alibaba/shop-user/src/main/java/com/UserApplication.java
+++ springcloud-alibaba/shop-user/src/main/java/com/UserApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @program: springcloud-alibaba
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description:
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class,args);
