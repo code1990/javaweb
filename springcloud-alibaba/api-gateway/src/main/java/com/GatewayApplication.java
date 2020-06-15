fc6fa4e5cbfa7ce8aec1fade9e5d7656c34b4211
code1990 <s1332177151@sina.com>
2020-06-15 22:57:42 +0800

    init

---
+++ springcloud-alibaba/api-gateway/src/main/java/com/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springcloud-alibaba
+ * @Date: 2020-06-15 22:52
+ * @Author: code1990
+ * @Description:
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
--- springcloud-alibaba/shop-order/src/main/java/com/service/ProductService.java
+++ springcloud-alibaba/shop-order/src/main/java/com/service/ProductService.java
@@ -2,6 +2,7 @@ package com.service;
 
 import com.Product;
 import com.service.fallback.ProductServiceFallBack;
+import com.service.fallback.ProductServiceFallBackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Author: code1990
  * @Description:
  */
-@FeignClient(value = "service-product",fallback = ProductServiceFallBack.class)
+@FeignClient(value = "service-product",
+//        fallback = ProductServiceFallBack.class
+        fallbackFactory = ProductServiceFallBackFactory.class
+)
 public interface ProductService {
 
     @RequestMapping("/product/{pid}")
